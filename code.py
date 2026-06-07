c9708cb6bd18b7ca4625770d099c44b6ba6ff780
Bill Eisenhower <bill.eisenhower@cirrus.com>
2026-06-06 22:30:01 -0500

    Create basic starting point with single trellis

--- code.py
+++ code.py
@@ -1,1 +1,11 @@
-print("Hello World!!!")
+import board
+import busio
+import digitalio
+import time
+from adafruit_neotrellis.neotrellis import NeoTrellis
+
+#create the i2c object for the trellis
+i2c_bus = busio.I2C(board.SCL, board.SDA)
+
+#create the trellis
+# trellis = NeoTrellis(i2c_bus, addr=0x2E)
