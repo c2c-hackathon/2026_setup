be6c848f44fe9dd43f3717210a530efdcff4077a
Bill Eisenhower <bill.eisenhower@cirrus.com>
2026-06-15 17:56:47 -0500

    Adding brightness control

--- disco.py
+++ disco.py
@@ -18,11 +18,23 @@ print(f"{i2c_bus.scan()=}")
 # Create the trellis
 trellis = NeoTrellis(i2c_bus, addr=0x2E)
 
+def brightness(event):
+    # Increase brightness
+    if event.number < 8:  # increase with top half
+        trellis.brightness = min(1.00, trellis.brightness + 0.05)
+    else:  # decrease with bottom half
+        trellis.brightness = max(0.05, trellis.brightness - 0.05)
+    print(f"{trellis.brightness=:.2f}")
+
 # Turn off all LEDs
 for i in range(16):
     trellis.pixels[i] = OFF
+    trellis.activate_key(i, NeoTrellis.EDGE_RISING)
+    # trellis.activate_key(i, NeoTrellis.EDGE_FALLING)
+    trellis.callbacks[i] = brightness
 
 while True:
+    trellis.sync()
     for i in range(16):
         trellis.pixels[i] = colorwheel(random.randrange(256))
 
