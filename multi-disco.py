875df5df02f4b457ad90d1e4c13c2848274ab0c2
Bill Eisenhower <bill.eisenhower@cirrus.com>
2026-06-18 17:39:58 -0500

    Failed interrupt test

--- multi-disco.py
+++ multi-disco.py
@@ -16,6 +16,10 @@ OFF = (0, 0, 0)
 i2c_bus = busio.I2C(board.SCL, board.SDA)
 print(f"{i2c_bus.scan()=}")
 
+int_pin = digitalio.DigitalInOut(board.D4)
+int_pin.direction = digitalio.Direction.INPUT
+print(f"{int_pin.value=}")
+
 # This is for a 2x2 array of NeoTrellis boards:
 trelli = [
     [NeoTrellis(i2c_bus, False, addr=0x2E, auto_write=False), NeoTrellis(i2c_bus, False, addr=0x2F, auto_write=False)],
@@ -41,11 +45,16 @@ for y in range(8):
 
 while True:
     try:
+        print(f"{int_pin.value=}")
+        time.sleep(1)
+        if int_pin.value is False:
+            print("Interrupt Low!")
         trellis.sync()
         for y in range(8):
             for x in range(8):
                 trellis.color(x, y, colorwheel(random.randrange(256)))
         trellis.show()
+
     except KeyboardInterrupt:
         # Turn off all LEDs
         print("\nLeaving the disco...")
