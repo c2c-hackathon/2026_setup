df5cb6fea524378f60ac4911f6bff53e85224203
c2c-hackathon <hackathon.c2c@gmail.com>
2026-07-30 13:43:28 -0500

    Added clearing of display and exit on Ctrl+C

--- StarterCode/main.py
+++ StarterCode/main.py
@@ -4,5 +4,13 @@ from NeoTrellisGame import NeoTrellisGame
 game = ConnectFour()
 game.update_display()
 while True:
-    game.board.sync()
-    pass
+    try:
+        game.board.sync()
+    except KeyboardInterrupt:
+        # clear board
+        print("\nClosing game...")
+        for x in range(8):
+            for y in range(8):
+                game.set_cell_color(x, y, [0, 0, 0])
+        game.update_display()
+        exit()  # quit
