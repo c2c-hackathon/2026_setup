d4dc0cfa51b25160b1b7d8a9aa481795ae90f628
Matthew Shafer <matthew.shafer@emerson.com>
2026-09-04 15:46:42 -0500

    don't need to convert, it's already an int

--- StarterCode/ConnectFour.py
+++ StarterCode/ConnectFour.py
@@ -59,7 +59,7 @@ class ConnectFour(NeoTrellisGame):
         return -1 # -1 means the column is full
 
     def drop_piece(self, col: int):
-        self.game_state[self.find_lowest_empty_row(col)][col] = CellState(self.current_player)
+        self.game_state[self.find_lowest_empty_row(col)][col] = self.current_player
         self.update_board_colors()
         win, winning_sequence = self.check_win()
         tie = self.board_is_full() 
