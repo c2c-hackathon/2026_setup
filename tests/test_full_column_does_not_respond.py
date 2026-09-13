0c9251583ce8b3003ea87176001060413b052f67
Matthew Shafer <matthew.shafer@emerson.com>
2026-09-12 21:27:53 -0500

    little bit of cleanup

--- tests/test_full_column_does_not_respond.py
+++ tests/test_full_column_does_not_respond.py
@@ -5,10 +5,8 @@ import tests._utils
 
 def test___column_is_full___same_column_pressed___extra_move_is_rejected(game_and_board):
     game, board = game_and_board
-
     for _ in range(6):
         assert board.press(0, 0)
-
     colors_before_extra_press = dict(board.colors)
 
     accepted = board.press(0, 0)
@@ -18,7 +16,7 @@ def test___column_is_full___same_column_pressed___extra_move_is_rejected(game_an
     assert board.colors == colors_before_extra_press
 
 
-def player_token_for_state(connect_four_module, current_player_state):
+def _player_token_for_state(connect_four_module, current_player_state):
     if current_player_state == connect_four_module.CellState.PLAYER_1:
         return "1"
     return "2"
@@ -33,7 +31,7 @@ def test___column_is_full___same_column_pressed___turn_indicator_stays_on_curren
     for _ in range(6):
         board.press(0, 0)
 
-    expected_player = player_token_for_state(connect_four_module, game.current_player)
+    expected_player = _player_token_for_state(connect_four_module, game.current_player)
     expected_color = game.get_player_color(
         tests._utils.player_state(connect_four_module, expected_player)
     )
