bf29e276f3286c7323ea7c7d8c0e7f99a260a8ba
Matthew Shafer <matthew.shafer@emerson.com>
2026-09-12 21:25:21 -0500

    split this into two tests

--- tests/test_buttons_respond.py
+++ tests/test_buttons_respond.py
@@ -24,6 +24,14 @@ def test___new_game___column_pressed_twice___pieces_stack_and_turn_indicator_alt
     assert board.color_at(3, 7) == game.get_player_color(connect_four_module.CellState.PLAYER_1)
     assert board.color_at(0, 0) == game.get_player_color(connect_four_module.CellState.PLAYER_2)
 
+
+def test___game_with_one_piece___column_pressed___piece_drops_to_next_row_and_turn_indicator_alternates(
+    connect_four_module,
+    game_and_board,
+):
+    game, board = game_and_board
+    board.press(3, 0) # Player 1's turn, tested above, now Player 2's turn
+
     board.press(3, 0)
 
     assert board.color_at(3, 6) == game.get_player_color(connect_four_module.CellState.PLAYER_2)
