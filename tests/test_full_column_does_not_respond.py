6512d97fb3d9e291f22b134c827980ecd35dc07d
Matthew Shafer <matthew.shafer@emerson.com>
2026-09-18 20:27:01 -0500

    change column full testing to use get_player_color and board.color_at

--- tests/test_full_column_does_not_respond.py
+++ tests/test_full_column_does_not_respond.py
@@ -1,7 +1,5 @@
 """Component tests for Connect Four presses that should be rejected."""
 
-import tests._utils
-
 
 def test___column_is_full___same_column_pressed___extra_move_is_rejected(game_and_board):
     game, board = game_and_board
@@ -15,13 +13,6 @@ def test___column_is_full___same_column_pressed___extra_move_is_rejected(game_an
     assert board.key_states[(0, 0)]["enable"] is False
     assert board.colors == colors_before_extra_press
 
-
-def _player_token_for_state(connect_four_module, current_player_state):
-    if current_player_state == connect_four_module.CellState.PLAYER_1:
-        return "1"
-    return "2"
-
-
 def test___column_is_full___same_column_pressed___turn_indicator_stays_on_current_player(
     connect_four_module,
     game_and_board,
@@ -31,13 +22,13 @@ def test___column_is_full___same_column_pressed___turn_indicator_stays_on_curren
     for _ in range(6):
         board.press(0, 0)
 
-    expected_player = _player_token_for_state(connect_four_module, game.current_player)
-    expected_color = game.get_player_color(
-        tests._utils.player_state(connect_four_module, expected_player)
-    )
     indicator_color_before_extra_press = board.color_at(1, 0)
+    player_colors = {
+        game.get_player_color(connect_four_module.CellState.PLAYER_1),
+        game.get_player_color(connect_four_module.CellState.PLAYER_2),
+    }
 
     board.press(0, 0)
 
-    assert indicator_color_before_extra_press == expected_color
-    assert board.color_at(1, 0) == expected_color
+    assert indicator_color_before_extra_press in player_colors
+    assert board.color_at(1, 0) == indicator_color_before_extra_press
